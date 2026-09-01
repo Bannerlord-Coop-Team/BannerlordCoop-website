4478d3270ed6b316203b956b5ebf9e076879d271
Andrew Orlowski <57453461+ShoT-UPfps@users.noreply.github.com>
2026-09-01 15:28:56 -0500

    Fix Cloudflare Workers preview builds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".open-next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
